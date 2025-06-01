c7cb22b0a4faa5b131e9c4b0f1a5e2561d74d72f
sayandas-sd <sayandasmac@gmail.com>
2025-06-01 23:55:05 +0530

    added user authentication and check pubAddress

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,14 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const v1_1 = require("./routes/v1");
 const app = (0, express_1.default)();
 const port = 3000;
 app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.status(200).json({
-        msg: "hello guys"
+        msg: "server is working"
     });
 });
+app.use("/api/v1", v1_1.router);
 app.listen(port, () => {
-    console.log(`server is running in port${port}`);
+    console.log(`server is running in port : ${port}`);
 });
--- backend/src/routes/v1/router/user.ts
+++ backend/src/routes/v1/router/user.ts
@@ -1,14 +1,49 @@
 import {Router} from "express";
+import jwt from "jsonwebtoken";
+import { prisma } from "../../../db";
+import { JWT_SECRET } from "../../../config";
 
 export const authRouter = Router();
 
-authRouter.post('/signup', async (req, res) => {
-    res.status(200).json({
-        message: "successfully signup"
-    })
-})
 
-authRouter.post("/signin", (req, res) =>{
+authRouter.post("/signin", async (req, res) =>{
+
+    const pubAddress = "HtkgKvwwJdEwq3EpwwCtVcHqvZed1Davc1wCB4JQkzcZ";
+
+    const existingUser = await prisma.user.findFirst({
+        where: {
+            address: pubAddress
+        }
+    })
+
+    if(existingUser) {
+        const token =  jwt.sign({
+            userId: existingUser.id
+        },JWT_SECRET)
+
+        res.json({
+            token
+        })
+        
+    } else {
+        const user = await prisma.user.create({
+            data: {
+                address: pubAddress
+            }
+        })
+
+        const token =  jwt.sign({
+            userId: user.id
+        },JWT_SECRET)
+
+        res.json({
+            token: token
+        })
+    }
+
+
+
+
     res.status(200).json({
         message: "successfully signin"
     })
