307af684371df09c23a68fd0bcce50f893117448
sayandas-sd <sayandasmac@gmail.com>
2025-06-04 01:30:19 +0530

    added middleware to this presignedUrl route

---
+++ backend/src/middleware.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+
+export function authmiddleWare(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')) {
+        res.status(401).json({
+            message: "Unauthorized"
+        })
+        return;
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try{
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        //@ts-ignore
+        if(decoded.userId) { 
+        //@ts-ignore
+            req.userId = decoded.userId;
+            return next();
+            
+        } else {
+            res.status(403).json({
+            message: "You are not logged in"
+            })
+            return;
+        }
+    }catch(e) {
+        res.status(403).json({
+            message: "You are not logged in"
+        })
+        return;
+    }
+}
--- backend/src/routes/v1/router/user.ts
+++ backend/src/routes/v1/router/user.ts
@@ -2,53 +2,62 @@ import {Router} from "express";
 import jwt from "jsonwebtoken";
 import { prisma } from "../../../db";
 import { JWT_SECRET } from "../../../config";
+import { authmiddleWare } from "../../../middleware";
 
 export const authRouter = Router();
+
+
+authRouter.post("/presignedUrl", authmiddleWare, (req, res) => {
+   res.json({
+    msg: "succesffuly generate"
+   })
+   return;
+})
 
 
 authRouter.post("/signin", async (req, res) =>{
 
     try{
 
-    const pubAddress = "HtkgKvwwJdEwq3EpwwCtVcHqvZed1Davc1wCB4JQkzcZ";
+        const pubAddress = "HtkgKvwwJdEwq3EpwwCtVcHqvZed1Davc1wCB4JQkzcZ";
 
-    const existingUser = await prisma.user.findFirst({
-        where: {
-            address: pubAddress
-        }
-    })
-
-    if(existingUser) {
-        const token =  jwt.sign({
-            userId: existingUser.id
-        },JWT_SECRET)
-
-        res.json({
-            token
-        })
-        
-        return;
-        
-    } else {
-        const user = await prisma.user.create({
-            data: {
+        const existingUser = await prisma.user.findFirst({
+            where: {
                 address: pubAddress
             }
         })
 
-        const token =  jwt.sign({
-            userId: user.id
-        },JWT_SECRET)
+        if(existingUser) {           
+            const token =  jwt.sign({
+                userId: existingUser.id
+            },JWT_SECRET)
 
-        res.json({
-            token: token
-        })
-        
-        return;
-        
-    }
+            res.json({
+                token
+            })
+            
+            return;
+            
+        } else {
+            const user = await prisma.user.create({
+                data: {
+                    address: pubAddress
+                }
+            })
 
-    }catch(e) {
+            const token =  jwt.sign({
+                userId: user.id
+            },JWT_SECRET)
+
+            res.json({
+                token: token
+            })
+            
+            return;
+        
+        }
+
+}catch(e) {
         res.status(500).json({
             error: "Something went wrong"
         })
