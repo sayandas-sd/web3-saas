e602ef7cfc8910dae951c87de1a843ad2371def5
sayandas-sd <sayandasmac@gmail.com>
2025-06-09 14:30:55 +0530

    add user frontend

---
+++ client-side/components/Appbar.tsx
@@ -0,0 +1,11 @@
+
+export function Appbar() {
+
+    return <div className="cursor-pointer w-40 h-40 flex justify-center items-center">
+   
+            <div className="font-bold text-xl text-2l">
+                hello guys
+            </div>
+    
+    </div>
+}
