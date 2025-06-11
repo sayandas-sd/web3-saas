6eebe0b6827ddd949cf125c228fb7dd0cf757509
sayandas-sd <sayandasmac@gmail.com>
2025-06-11 23:18:45 +0530

    fix: cloudflare r2 key in config

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,3 +1,6 @@
 export const JWT_SECRET = process.env.JWT_SECRET || "123secret#";
 export const WORKER_JWT_SECRET = process.env.WORKER_JWT_SECRET || "naksnaksn";
 export const TOTAL_LAMPORTS_AMOUNT = 1000_000;
+export const CLOUDFLARE_ENDPOINT=process.env.CLOUDFLARE_ENDPOINT;
+export const S3_ACCESS_KEY=process.env.S3_ACCESS_KEY || '';
+export const S3_SECRET_KEY=process.env.S3_SECRET_KEY || '';
