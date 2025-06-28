3e14b8ca2df107cf1a93415b75ac412340ad4a53
sayandas-sd <sayandasmac@gmail.com>
2025-06-28 14:27:51 +0530

    feat: add docker compose and set env in config

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,22 +1,14 @@
-import dotenv from 'dotenv';
-dotenv.config(); 
+import dotenv from "dotenv";
+dotenv.config();
 
-export function requireEnvVar(name: string): string {
-  const value = process.env[name];
-  if (!value) {
-    throw new Error(`Environment variable ${name} is required but not set.`);
-  }
-  return value;
-}
-
-export const JWT_SECRET = requireEnvVar('JWT_SECRET');
-export const WORKER_JWT_SECRET = requireEnvVar('WORKER_JWT_SECRET');
-export const CLOUDFLARE_ENDPOINT = requireEnvVar('CLOUDFLARE_ENDPOINT');
-export const S3_ACCESS_KEY = requireEnvVar('S3_ACCESS_KEY');
-export const S3_SECRET_KEY = requireEnvVar('S3_SECRET_KEY');
-export const CLOUDFLARE_BUCKET = requireEnvVar('CLOUDFLARE_BUCKET');
-export const RPC_URL = requireEnvVar('RPC_URL');
-export const PRIVATE_KEY = requireEnvVar('PRIVATE_KEY');
+export const JWT_SECRET = process.env.JWT_SECRET || "";
+export const WORKER_JWT_SECRET = process.env.WORKER_JWT_SECRET || "";
+export const CLOUDFLARE_ENDPOINT = process.env.CLOUDFLARE_ENDPOINT || "";
+export const S3_ACCESS_KEY = process.env.S3_ACCESS_KEY || "";
+export const S3_SECRET_KEY = process.env.S3_SECRET_KEY || "";
+export const CLOUDFLARE_BUCKET = process.env.CLOUDFLARE_BUCKET || "";
+export const RPC_URL = process.env.RPC_URL || ""; 
+export const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 
-export const TOTAL_LAMPORTS_AMOUNT = 1000_000;
+export const TOTAL_LAMPORTS_AMOUNT = 1_000_000; 
