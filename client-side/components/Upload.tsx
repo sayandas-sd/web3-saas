3e8abc40b82024f10765ef717d6b8450a79fabc8
sayandas-sd <sayandasmac@gmail.com>
2025-10-09 22:06:04 +0530

    fix: adjust text color with theme

--- client-side/components/Upload.tsx
+++ client-side/components/Upload.tsx
@@ -72,14 +72,14 @@ export const Upload = () => {
                 Create a task
             </div>
 
-            <label className="pl-4 block mt-2 text-md font-medium text-gray-900 text-black">Task details</label>
+            <label className="pl-4 block mt-2 text-md font-medium">Task details</label>
 
             <input onChange={(e) => {
                 setTitle(e.target.value);
             }} type="text" id="first_name" className="ml-4 mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="What is your task?" required />
 
 
-            <label className="pl-4 block mt-8 text-md font-medium text-gray-900 text-black">Add Images</label>
+            <label className="pl-4 block mt-8 text-md font-medium">Add Images</label>
 
             <div className="flex justify-center pt-4 max-w-screen-lg">
                 {images.map((image, index) => 
